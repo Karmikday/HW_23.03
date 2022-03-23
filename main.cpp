f452bce5f38c8b23e5f9903b2b465e500e7c4a38
Tata_kn18 <karmikday@gmail.com>
2022-03-23 19:35:05 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+int stars(int n) 
+{ 
+	if (n == 0)
+	{
+		return 1;
+	}
+	stars(n - 1); 
+	cout << " * "; 
+}
 int main()
 {
-
+	int n;
+	cout << "How much stars->";
+	cin >> n;
+	stars(n);
 
 }
