ca23b093216a4cfebe6f88638b7dd45e6fafa4fc
Tata_kn18 <karmikday@gmail.com>
2022-03-23 19:45:44 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
+int  summa(int x, int n)  
+{
+    if (n == x - 1)
+    {
+        return 0;
+    }
+    return n + summa(x, n-1);
+}
 int main()
 {
+    int x, y;
+    cout << "enter a range. 1 num->";
+    cin >> x;
+    cout << "enter a range. 2 num->";
+    cin >> y;
+    cout << summa(x, y);
 
 
 }
