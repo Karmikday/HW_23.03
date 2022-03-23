234342f276c1b37ff0f700a897b48b9d71fa9b29
Tata_kn18 <karmikday@gmail.com>
2022-03-23 19:26:14 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
+int degree(int n, int x)
+{
+    if (x == 1)
+    {
+        return n;
+    }
+    if (!x)
+    {
+        return 1;
+    }
+    else if (n < 0)
+    {
+        return 1;
+    }
+    return n * degree(n, --x);
+}
 int main()
 {
-
+    int n;
+    int x;
+    cout << "Your num->";
+    cin >> n;
+    cout << "Degree->";
+    cin >> x;
+    cout << "answer->" << degree(n, x);
 
 }
